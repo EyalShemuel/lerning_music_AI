ece79daa507abdafbcb79df83a00c1ced187a264
eyal home <eshemuel1@gmail.com>
2021-03-06 21:31:24 +0200

    1

--- client/src/Components/Notes/Notes.js
+++ client/src/Components/Notes/Notes.js
@@ -1,8 +1,8 @@
-import React, {useEffect } from "react";
+import React from "react";
 import "./notes.css";
-import Vex from 'vexflow';
-import { Score } from './react-vexflow'
- const VF = Vex.Flow;
+//import Vex from 'vexflow';
+import Myvexflow from './Myvexflow';
+ //const VF = Vex.Flow;
 
 
 
@@ -11,7 +11,9 @@ const Notes = (props) => {
  
   
   
- /*  useEffect(() => {
+ /* 
+ vaxflow example
+ useEffect(() => {
     const vf = new VF.Factory({
       renderer: {elementId: 'boo', width: 1600, height: 200}
     });
@@ -30,33 +32,27 @@ const Notes = (props) => {
   
   },[]);
    */
-  
-  
+  const notesChanks = [...props.fiestNotesObject];
+
+
+   
+
+const longArray = [...notesChanks];  
+let shortArrays = [];
+
+
+for (let i = 0, len = longArray.length; i < len; i += 4) {
+    shortArrays.push(longArray.slice(i, i + 4));
+}
   return (
-
   <>
-   <Score
-     width='1200'
-     height='300'
-     clef = 'treble'
-     staves={[
-      ['g3', 'd4', 'e4', 'd4'],
-    ['a4', 'd4', 'e4', 'd4'],
-    ['a4', 'd4', 'e4', 'd4'],
-    ['a4', 'd4', 'e4', 'd4'],
-    ['a4', 'd4', 'e4', 'd4'],
-    ['a4', 'd4', 'e4', 'd4'],
-    ['a4', 'a4', 'b4', 'a4'],
-    ['d4', 'e4', ['g3', 2]],
-     ]}
-     
-   />
-
-    
+  <Myvexflow width='1200' height='150' clef = 'treble' staves={notesChanks}/>
+  <Myvexflow width='1200' height='150' clef = 'treble' staves={notesChanks}/>
+  {/* {shortArrays.forEach((array)=>{
+    console.log(<Myvexflow width='1200' height='150' clef = 'treble' staves={array}/>)
+   })} */}
+   
   </>
-    
-    
-
   );
 };
 
