c5930424dc08b314fb48f49e8efa06dc6f354cba
EyalShemuel <70960250+EyalShemuel@users.noreply.github.com>
2021-03-12 09:58:55 +0200

    Update KeyBord.js

--- client/src/components/KeyBord/KeyBord.js
+++ client/src/components/KeyBord/KeyBord.js
@@ -4,8 +4,16 @@ import Key from './Key';
 /* key, note, type, pressed */
 
 const KeyBord = ({keys,notesObject,setNotesObject}) => {
+  const sheetNote = [];
+
   useEffect(() => {
-    /* {key:'a',note:'c1',pressed:false} */
+    ////set querySelectors ////////////
+    const keyDivs = document.querySelectorAll('.key');
+    const whiteKeys = document.querySelectorAll('.key.white');
+    const blackKeys = document.querySelectorAll('.key.black');
+    ////end set querySelectors///////////
+
+    /* ************************************** */
 
     ///get a list of the white and black keys///////
     const WHITE_KEYS = keys
@@ -16,9 +24,9 @@ const KeyBord = ({keys,notesObject,setNotesObject}) => {
       .map((key) => key.key);
     ////////////////////////////////////////////
 
-    const keyDivs = document.querySelectorAll('.key');
-    const whiteKeys = document.querySelectorAll('.key.white');
-    const blackKeys = document.querySelectorAll('.key.black');
+      /* ************************************** */
+
+
 
     keyDivs.forEach((key) => {
       key.addEventListener('click', (e) => console.log(e));
@@ -26,45 +34,25 @@ const KeyBord = ({keys,notesObject,setNotesObject}) => {
 
     document.addEventListener('keydown', (e) => {
       if (e.repeat) return;
-
-      //console.log(keyPressed);
-      const key = e.key;
-      const whiteKeyIndex = WHITE_KEYS.indexOf(key);
-      const blackKeyIndex = BLACK_KEYS.indexOf(key);
-
-      if (whiteKeyIndex > -1) {
-        whiteKeys[whiteKeyIndex].classList.add('active');
-      }
-      if (blackKeyIndex > -1) blackKeys[blackKeyIndex].classList.add('active');
+      keyPressSimuletion(e, WHITE_KEYS, BLACK_KEYS, whiteKeys, blackKeys,'add');     
     });
 
     document.addEventListener('keyup', (e) => {
-      const keyPressed = keys
+      const noteToAdd = keys
         .filter((key) => (key.key === e.key ? key.note : ''))
         .map((key) => key.note);
-      if (e.repeat) return;
-      const key = e.key;
-      const whiteKeyIndex = WHITE_KEYS.indexOf(key);
-      const blackKeyIndex = BLACK_KEYS.indexOf(key);
-
-      if (whiteKeyIndex > -1) {
-        whiteKeys[whiteKeyIndex].classList.remove('active');        
-      }
-      if (blackKeyIndex > -1)
-        blackKeys[blackKeyIndex].classList.remove('active');
-        editNotes(keyPressed);
+      
+        keyPressSimuletion(e, WHITE_KEYS, BLACK_KEYS, whiteKeys, blackKeys,'remove');
+        editNotes(noteToAdd,sheetNote);
     });
    // eslint-disable-next-line 
   }, []);
 
-  const sheetNote = [];
-  const editNotes = (note) => {
-    sheetNote.push(note[0]);
-
-    console.log(note[0]);
-    setNotesObject([...notesObject, sheetNote]);
-  };
+ 
+  
   console.log(notesObject);
+
+
   return (
     <div className="piano">
       {keys.map((key,index) => (
@@ -73,9 +61,7 @@ const KeyBord = ({keys,notesObject,setNotesObject}) => {
           theKey={key.key}
           note={key.note}
           type={key.type}
-          pressed={key.pressed}         
-          onKeyDown={()=>console.log('im down')}
-          onKeyUp={()=>console.log('im up')}
+          pressed={key.pressed} 
         />
       ))}
     </div>
@@ -83,3 +69,27 @@ const KeyBord = ({keys,notesObject,setNotesObject}) => {
 };
 
 export default KeyBord;
+
+const editNotes = (note,sheetNote) => {
+  sheetNote.push(note[0]);
+  //console.log(note[0]);
+  setNotesObject([...notesObject, sheetNote]);
+};
+
+const keyPressSimuletion = (e, WHITE_KEYS, BLACK_KEYS, whiteKeys, blackKeys,AddOrRemoveclass) => {
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+  if (AddOrRemoveclass = 'add') {
+    if (whiteKeyIndex > -1)
+      whiteKeys[whiteKeyIndex].classList.add('active');
+    if (blackKeyIndex > -1)
+      blackKeys[blackKeyIndex].classList.add('active');
+  } else if (AddOrRemoveclass = 'remove') {
+    if (whiteKeyIndex > -1)
+      whiteKeys[whiteKeyIndex].classList.remove('active');
+    if (blackKeyIndex > -1)
+      blackKeys[blackKeyIndex].classList.remove('active');
+  }
+}
+
